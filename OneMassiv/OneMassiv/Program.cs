3320eac4ca69c097566cf3457ca3d7057d7623c1
Alex-Sweft1234 <babaev-a-2010@yandex.ru>
2017-04-05 20:31:26 +0300

    Работа с одномерными массивами

---
+++ OneMassiv/OneMassiv/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneMassiv
+{
+    public class Massiv
+    {
+        const int n = 5;
+        double[] mas = new double[n];
+        Random r = new Random();
+        Random k = new Random();
+        public double[] InMas()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                mas[i] = k.Next(-10, 10) * r.NextDouble();
+
+            }
+            return mas;
+        }
+        
+        public double SumOtr()
+        {
+            double x = 0;
+            InMas();
+            Array.Sort(mas);
+            foreach (double s in mas)
+            {
+                Console.WriteLine(s);
+            }
+            for (int i = 0; i < n; i++)
+            {
+                if (mas[i] < 0)
+                {
+                    x += mas[i];
+                }
+            }
+            return x;
+        }
+        public double UmValue()
+        {
+            double w = 1;
+            InMas();
+            Array.Sort(mas);
+            foreach (double s in mas)
+            {
+                Console.WriteLine(s);
+            }
+            for (int i = 1; i < n-1; i++)
+            {
+                w *= mas[i];
+            }
+            return w;
+        }
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Massiv c = new Massiv();
+            double d = c.SumOtr();
+            Console.WriteLine("Сумма отрицательных элементов: {0}", d);
+            double p = c.UmValue();
+            Console.WriteLine("Произведение элементов от минимального до максимального: {0}", p);
+            Console.ReadKey();
+        }
+     }
+    
+}
